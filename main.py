6d2655e1e05b5be9f25da43cd8560eb80bfbcb23
nam3dan <namillennial@gmail.com>
2023-02-23 09:39:01 -0600

    I

--- main.py
+++ main.py
@@ -54,34 +54,37 @@ def food_search(city_name):
 	food_selection, food_list = selection_rotator("Dining Experience", food_list)
 	return(food_selection, food_list)
 
-def entertainment_search(city_name):
-	print("\nExcellent Choice! Now Let's look into Entertainment!\n")
-	query_term = city_name.replace(', ','%2C+')
-	url = 'https://www.yelp.com/search?find_desc=Entertainment&find_loc=' + query_term
+def real_venue_search(city_name, mode):
+	print("\nExcellent Choice! Now Let's look into %s!\n" % mode)
+	query_term = city_name.replace(', ','%2C+').replace(' ','%2C+')
+	url = ('https://www.yelp.com/search?find_desc=%s&find_loc=' % mode) + query_term
 	headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36', 'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'}
 	response = requests.request("GET", url, headers=headers)
 	tree = html.fromstring(response.content)
-	entertainment_list = []
+	venue_list = []
+	xpath_list = ['//*[@id="main-content"]/div/ul/li[',']/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/div/div/h3/span/a/text()',']/div/div/div/div[2]/div[1]/div/div[1]/div/div[1]/div/div/h3/span/a/text()']
+	if mode == "Restaurant":
+		xpath_2 = xpath_list[1]
+	else:
+		xpath_2 = xpath_list[2]
 	try:
 		for i in range(3,12):
-			xpath_query_entertainment = '//*[@id="main-content"]/div/ul/li['+ str(i) +']/div/div/div/div[2]/div[1]/div/div[1]/div/div[1]/div/div/h3/span/a/text()'
-						#//*[@id="main-content"]/div/ul/li[3]/div/div/div/div[2]/div[1]/div/div[1]/div/div[1]/div/div/h3/span/a
-			venue = tree.xpath(xpath_query_entertainment)[0].replace("â\x80\x99","'")
-			entertainment_list.append(venue)
+			xpath_query = xpath_list[0] + str(i) + xpath_2
+			restaurant = tree.xpath(xpath_query)[0].replace("â\x80\x99","'")
+			venue_list.append(restaurant)
 	except:
-		for i in range(8,17):
-			xpath_query_entertainment = '//*[@id="main-content"]/div/ul/li['+ str(i) +']/div/div/div/div[2]/div[1]/div/div[1]/div/div[1]/div/div/h3/span/a/text()'
-						#//*[@id="main-content"]/div/ul/li[3]/div/div/div/div[2]/div[1]/div/div[1]/div/div[1]/div/div/h3/span/a
-			venue = tree.xpath(xpath_query_entertainment)[0].replace("â\x80\x99","'")
-			entertainment_list.append(venue)
-	entertainment_selection, entertainment_list = selection_rotator("Entertainment", entertainment_list)
-	return(entertainment_selection, entertainment_list)
+		for i in range(8,16):
+			xpath_query = xpath_list[0] + str(i) + xpath_2
+			restaurant = tree.xpath(xpath_query)[0].replace("â\x80\x99","'")
+			venue_list.append(restaurant)
+	venue_selection, venue_list = selection_rotator("%s Experience" % mode, venue_list)
+	return(venue_selection, venue_list)
 
 def greeting():
 	city_list = get_destinations()
 	print("\n\n\n\n\n\n\n\nHello and Welcome to nam3dan's Dope Ass Travel Agency")
 	city_search_choice = False
-	while city_search_choice == False:s
+	while city_search_choice == False:
 		city_search_choice_selection = int(input("\n\nWould you like to 1)look through our list of Citys or 2) do a custom search? Please respond with either a 1 or 2.: "))
 		if city_search_choice_selection == 1:
 			print("\n Generating a list of Top Travel Destinations")
